6a61ae38aca923ad762e0e868d596452e51ba41d
Fritz Ammon <redmage1993@gmail.com>
2017-10-22 22:39:50 -0700

    initial attempt at bulk save for later

--- js/bulkSaveForLater.js
+++ js/bulkSaveForLater.js
@@ -1,13 +1,27 @@
 var amazonUIPage = window.AmazonUIPageJS || window.P;
 
+var bulkSaveForLaterIntervalID = 0;
+function bulkSaveForLater($, cartBaseView) {
+  var $saveForLaterLinks = $("input[name^=submit\\.save-for-later]");
+
+  if ($saveForLaterLinks.length == 0) {
+    console.log("BulkSaveForLater: Done.");
+    clearInterval(bulkSaveForLaterIntervalID);
+    return;
+  }
+
+  $saveForLaterLinks.each(function(index, saveForLaterLink) {
+    var $saveForLaterLink = $(saveForLaterLink);
+    var itemID = $saveForLaterLink.attr('name').split('.')[2];
+
+    cartBaseView.saveItemForLater(itemID, 'ox_sc_mtsfl_1');
+  });
+}
+
 amazonUIPage.when("jQuery", "CartBaseView", "ready").execute(function($, cartBaseView) {
   $('form#activeCartViewForm input[name=submit\\.save-all-for-later]').click(function(ev) {
     ev.preventDefault();
-
-    $("input[name^=submit\\.save-for-later]").each(function(index, saveForLaterLink) {
-      var $saveForLaterLink = $(saveForLaterLink);
-
-      console.log($saveForLaterLink.attr('name'));
-    });
+    console.log("BulkSaveForLater: Starting process.");
+    bulkSaveForLaterIntervalID = setInterval(bulkSaveForLater, 1000, $, cartBaseView);
   });
 });
