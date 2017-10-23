814da367781db4389a429736c0d851d6afdfcee7
Fritz Ammon <redmage1993@gmail.com>
2017-10-22 19:56:27 -0700

    inject 'save all for later link' into page

--- js/bulkSaveForLater.js
+++ js/bulkSaveForLater.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-  console.log("bulkSaveForLater: hello world");
+  function injectLink() {
+    if ($("form#activeCartViewForm input[name=submit\\.save-all-for-later]").size) {
+      return;
+    }
+
+    var linkHTML =
+      "<div class=\"sc-action-links a-text-left a-float-left\">" +
+      "  <p class=\"a-spacing-none a-spacing-top-mini\">" +
+      "    <span class=\"a-size-medium\">" +
+      "      <input type=\"submit\" name=\"submit.save-all-for-later\" value=\"Save all for later\">" +
+      "    </span>" +
+      "  </p>" +
+      "</div>";
+
+    $("form#activeCartViewForm .sc-subtotal").after(linkHTML);
+  }
+
+  injectLink();
 });
